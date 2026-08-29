7d4fa92de2e1da5927edc41f65a2c63a5f6b0de9
crengenhariarh-cyber <crengenharia.rh@gmail.com>
2026-08-29 09:09:45 -0300

    feat(finance): extend card repository for statements

--- src/modules/finance/application/CardRepository.ts
+++ src/modules/finance/application/CardRepository.ts
@@ -1,15 +1,23 @@
 import type { CompanyScope } from '../domain/registries';
 import type {
   CardInstallment,
+  CardStatementBalance,
+  CloseCardStatement,
+  ClosedCardStatement,
   CreateCardPurchase,
   CreatedCardPurchase,
   CreditCard,
   CreditCardLimit,
+  RecordCardStatementPayment,
+  RecordedCardStatementPayment,
 } from '../domain/cards';
 
 export interface CardRepository {
   listCards(scope: CompanyScope): Promise<readonly CreditCard[]>;
   listLimits(scope: CompanyScope): Promise<readonly CreditCardLimit[]>;
   listInstallments(scope: CompanyScope, cardId?: string): Promise<readonly CardInstallment[]>;
+  listStatements(scope: CompanyScope, cardId?: string): Promise<readonly CardStatementBalance[]>;
   createPurchase(input: CreateCardPurchase): Promise<CreatedCardPurchase>;
+  closeStatement(input: CloseCardStatement): Promise<ClosedCardStatement>;
+  recordStatementPayment(input: RecordCardStatementPayment): Promise<RecordedCardStatementPayment>;
 }
