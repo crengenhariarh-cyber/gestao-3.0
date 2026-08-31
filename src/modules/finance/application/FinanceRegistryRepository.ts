8947d8694e6bfa63d57f8dc4b35483a0f9553aa4
crengenhariarh-cyber <crengenharia.rh@gmail.com>
2026-08-30 21:29:47 -0300

    fix(finance): expose registry update operations

--- src/modules/finance/application/FinanceRegistryRepository.ts
+++ src/modules/finance/application/FinanceRegistryRepository.ts
@@ -6,15 +6,21 @@ import type {
   CreateFinancialCategory,
   FinancialAccount,
   FinancialCategory,
+  UpdateCostCenter,
+  UpdateFinancialAccount,
+  UpdateFinancialCategory,
 } from '../domain/registries';
 
 export interface FinanceRegistryRepository {
   listCategories(scope: CompanyScope): Promise<readonly FinancialCategory[]>;
   createCategory(input: CreateFinancialCategory): Promise<FinancialCategory>;
+  updateCategory(input: UpdateFinancialCategory): Promise<FinancialCategory>;
 
   listCostCenters(scope: CompanyScope): Promise<readonly CostCenter[]>;
   createCostCenter(input: CreateCostCenter): Promise<CostCenter>;
+  updateCostCenter(input: UpdateCostCenter): Promise<CostCenter>;
 
   listAccounts(scope: CompanyScope): Promise<readonly FinancialAccount[]>;
   createAccount(input: CreateFinancialAccount): Promise<FinancialAccount>;
+  updateAccount(input: UpdateFinancialAccount): Promise<FinancialAccount>;
 }
