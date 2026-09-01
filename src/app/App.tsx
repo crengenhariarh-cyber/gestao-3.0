97ab0b5a83595b3b8f0201ff3c45f5b1e9402eea
crengenhariarh-cyber <crengenharia.rh@gmail.com>
2026-09-01 10:50:01 -0300

    Carregar acabamento final da Home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { AppShell } from './AppShell';
 import './shell.css';
 import './central-menu.css';
 import './final.css';
+import './home-polish.css';
 
 export function App() {
   const session = usePlatformSession();
