6fa6cc1af38aab3eb4cb9ee1b1c28863d60ac78d
crengenhariarh-cyber <crengenharia.rh@gmail.com>
2026-08-31 21:53:02 -0300

    feat(finance): permitir planejar conta do lancamento

--- src/modules/finance/application/FinancialEntryRepository.ts
+++ src/modules/finance/application/FinancialEntryRepository.ts
@@ -10,5 +10,6 @@ export interface FinancialEntryRepository {
   createSingle(input: CreateSingleFinancialEntry): Promise<CreatedSingleFinancialEntry>;
   update(input: UpdateFinancialEntry): Promise<void>;
   deleteUnsettled(scope: CompanyScope, entryId: string): Promise<void>;
+  setPlannedAccount(scope: CompanyScope, entryId: string, accountId: string | null): Promise<void>;
   list(scope: CompanyScope): Promise<readonly FinancialEntryListItem[]>;
 }
